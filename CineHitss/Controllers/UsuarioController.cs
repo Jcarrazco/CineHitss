874803293b6471154b5d6a1c0b73ede6d4806c81
Carlos Sebastian Parra Lugo <parracs@globalhitss.com>
2019-05-03 11:04:11 -0500

    user  llenar

--- CineHitss/Controllers/UsuarioController.cs
+++ CineHitss/Controllers/UsuarioController.cs
@@ -11,6 +11,17 @@ namespace CineHitss.Controllers
         // GET: Usuario
         public ActionResult Index()
         {
+            User xuser = new User();
+            xuser.Username = "Juan";
+            xuser.Email = "juan@globalhitss.com";
+            xuser.Points = 2345;
+           
+
+            Session["Login"] = xuser;
+            User user2 = (User)Session["Login"];
+
+            ViewBag.login = xuser;
+            
             return View();
         }
 
