0215948123907df1d086788ccaca32c77901f4ea
carrascoja <carrascoja@HGDLAPCARRASCOJ>
2019-05-03 09:15:05 -0500

    Add Entity framework to MVC project

--- CineHitss/Controllers/FiltrosController.cs
+++ CineHitss/Controllers/FiltrosController.cs
@@ -11,7 +11,12 @@ namespace CineHitss.Controllers
         // GET: Filtros
         public ActionResult Index()
         {
-            return View();
+            using (var context = new CineHitssProxy())
+            {
+                context.Configuration.LazyLoadingEnabled = false;
+                context.Configuration.ProxyCreationEnabled = false;
+            }
+                return View();
         }
     }
 }
