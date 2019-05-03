93348d5365e80a8c41a628b89078dc30021bab18
calderonal <calderonal@HGDLAPCALDERONA>
2019-05-03 12:26:28 -0500

    Agrege lista y metodo para obtener ciudades y cines y modifique index

--- CineHitss/Controllers/FiltrosController.cs
+++ CineHitss/Controllers/FiltrosController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CineHitss.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +12,9 @@ namespace CineHitss.Controllers
         // GET: Filtros
         public ActionResult Index()
         {
-            return View();
+            Filtro filtro = new Filtro();
+
+            return View(filtro);
         }
     }
 }
---
+++ CineHitss/Models/Filtro.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CineHitssApi;
+
+namespace CineHitss.Models
+{
+    public class Filtro
+    {
+        public List<Ciudade> Ciudades { get; set; }
+        public List<Cine> Cines { get; set; }
+
+        public List<Cine> ObtenerCines(int id)
+        {
+            return Cines.Where(c => c.CiudadID == id).ToList();
+        }
+    }
+}
