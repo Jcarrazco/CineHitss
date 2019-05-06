7417239d934cb05480c1374d6b77bf2107d9ca77
Carlos Sebastian Parra Lugo <parracs@globalhitss.com>
2019-05-06 17:37:59 -0500

    Cambios minimos a usuario

--- CineHitss/Controllers/UsuarioController.cs
+++ CineHitss/Controllers/UsuarioController.cs
@@ -17,9 +17,13 @@ namespace CineHitss.Controllers
             var context = new DataBaseCineHitssEntities();
             IEnumerable<Historial> hist = new List<Historial>();
             User xuser = new User();
+            User user = new User();
             Pelicula peli = new Pelicula();
             List<lala> list = new List<lala>();
-          
+
+            TempData["puntos"] = 200;
+            int puntos = int.Parse(TempData["puntos"].ToString());
+
             CineHitssService.User user2 = (CineHitssService.User)Session["Login"];
 
             if (user2 == null)
@@ -38,7 +42,14 @@ namespace CineHitss.Controllers
                     list.Add(new lala { nombre = peli.Nombre, horario = cart.Horario});
                 }
             }
-             
+
+            context.Configuration.LazyLoadingEnabled = false;
+            context.Configuration.ProxyCreationEnabled = false;
+            user = context.Users.Find(ViewBag.login.id);
+            user.Points = ViewBag.login.Points + puntos; 
+            context.SaveChanges();
+
+            ViewBag.login.Points = ViewBag.login.Points + puntos;
             ViewBag.historia = list;
             return View();
         }
